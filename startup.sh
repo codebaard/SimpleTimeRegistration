1c976c967106ac8750f7b80d017425835c3aae1a
Julius Neudecker <Github@juliusneudecker.com>
2021-01-09 00:47:02 +0100

    bugfix

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 echo "Creating Secret..."
-echo $(python -c 'import os; print(os.urandom(16))') > config.py
+echo $(python -c 'import os; print(os.urandom(16))') > ./instance/config.py
 
 echo "Startig webserver..."
 waitress-serve --call 'app:create_app'
