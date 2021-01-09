baf42544f067d951a0bd1e6a4a462850fe518dd8
Julius Neudecker <Github@juliusneudecker.com>
2021-01-09 13:09:39 +0100

    startup script bugfix

--- startup.sh
+++ startup.sh
@@ -8,6 +8,13 @@ else
     echo $(python -c 'import os; print(os.urandom(16))') > ./instance/config.py
 fi
 
+DATABASE=/instance/app.sqlite
+if test -f "$DATABASE"; then
+    echo "$DATABSE already exists."
+else
+    echo "Creating Database..."
+    echo $(app init-db)
+
 echo "Startig webserver..."
 echo $(waitress-serve --call 'app:create_app')
 
